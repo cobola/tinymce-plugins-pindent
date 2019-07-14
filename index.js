2cad30c9b7dba2d6b6ab59610aded2d88d702d1f
yangjf <cobola@126.com>
2019-07-14 11:27:40 +0800

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+// Exports the "pindent" plugin for usage with module loaders
+// Usage:
+//   CommonJS:
+//     require('tinymce/plugins/pindent')
+//   ES2015:
+//     import 'tinymce/plugins/pindent'
+require('./plugin.js')
